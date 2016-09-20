6f1c5d49006b25570b5d66c8647a2cedf417d9f7
Joaquim Maia <joaquim.cmh@gmail.com>
2016-09-20 10:42:28 -0300

    Finalizado

--- semantic_similarity.py
+++ semantic_similarity.py
@@ -1,12 +1,61 @@
-######
+# usado para importar o csv
+import csv
+# Usado para fazer o tfidf
 from sklearn.feature_extraction.text import TfidfVectorizer
-######
+# diretorio dataset
+directory_dataset = 'data_academia.csv'
 
+# MENSAGEM DE QUANDO DA TUDO CERTO O//
+def ok ():
+    print("----------")
+    print("OK.")
+    print("----------")
 
-vect = TfidfVectorizer(min_df=1)
-tfidf = vect.fit_transform(["I like cars",
-                             "im an albatroz",
-                            "Sweet Child o mine"])
-print((tfidf * tfidf.T).A)
+def calc_tfidf():
+    with open('data_academia_results.csv', 'w') as csvfile:
+        # fields do csv
+        fieldnames = ['id_question',
+                      'answer_count',
+                      'AcceptedAnswerId',
+                      'body_question',
+                      'id_author_question',
+                      'name_author_question',
+                      'reputation_author_question',
+                      'id_answer',
+                      'body_answer',
+                      'id_author_answer',
+                      'name_author_answer',
+                      'reputation_author_answer',
+                      'score_answer',
+                      'id_parent',
+                      'accepted_answer',
+                      'tf_idf']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-print ("Rodou")
+        with open(directory_dataset) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                vect = TfidfVectorizer(min_df=1)
+                list = [row['body_question'], row['body_answer']]
+                tfidf = vect.fit_transform(list)
+                result = (tfidf * tfidf.T).A
+                writer.writerow({'id_question': row['id_question'],
+                                 'answer_count': row['answer_count'],
+                                 'AcceptedAnswerId': row['AcceptedAnswerId'],
+                                 'body_question': row['body_question'],
+                                 'id_author_question': row['id_author_question'],
+                                 'name_author_question': row['name_author_question'],
+                                 'reputation_author_question': row['reputation_author_question'],
+                                 'id_answer': row['id_answer'],
+                                 'body_answer': row['body_answer'],
+                                 'id_author_answer': row['id_author_answer'],
+                                 'name_author_answer': row['name_author_answer'],
+                                 'reputation_author_answer': row['reputation_author_answer'],
+                                 'score_answer': row['score_answer'],
+                                 'id_parent': row['id_parent'],
+                                 'accepted_answer': row['accepted_answer'],
+                                 'tf_idf': result[0][1]})
+
+calc_tfidf()
+
